33b15c8c003cbf86b78375d5bd2b8ce3e7eb8528
Algogence Technologies Private Limited <info@algogence.com>
2023-06-25 19:16:52 +0530

    android 6

--- app/src/main/java/com/algogence/ourtodo/MainActivity.kt
+++ app/src/main/java/com/algogence/ourtodo/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.algogence.ourtodo
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -20,19 +21,32 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -42,22 +56,32 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
+import com.algogence.ourtodo.mypackage.Task
 import com.algogence.ourtodo.ui.theme.OurTodoTheme
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    val iid = mutableStateOf(0)
+    val store = mutableStateListOf<Task>()
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -67,25 +91,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    IntroPage()
+                    Column(){
+                        val navController = rememberNavController()
+                        NavHost(navController = navController, startDestination = "splash") {
+                            composable("splash") { SplashPage(navController) }
+                            composable("intro") { IntroPage(navController) }
+                            composable("home") { HomePage(navController) }
+                            composable("add") { AddTask(navController) }
+                        }
+                    }
                 }
             }
         }
     }
 
     @Composable
-    private fun SplashPage() {
+    private fun SplashPage(navController: NavHostController) {
+        LaunchedEffect(key1 = Unit){
+            delay(1700)
+            navController.navigate("intro")
+        }
         Box(
             contentAlignment = Alignment.Center
         ){
-            //AnimatedLogo()
             Loader()
         }
     }
 
     @OptIn(ExperimentalFoundationApi::class)
     @Composable
-    fun IntroPage(){
+    fun IntroPage(navController: NavHostController) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ){
@@ -135,6 +170,171 @@ class MainActivity : ComponentActivity() {
                         state.animateScrollToPage(2)
                     }
                 }
+            }
+            var introSeen by remember {
+                mutableStateOf(false)
+            }
+            LaunchedEffect(key1 = state.currentPage){
+                if(state.currentPage==2){
+                    introSeen = true
+                }
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+            Button(onClick = {
+                navController.navigate("home")
+            }) {
+                Text(if(introSeen) "Get Started" else "Skip")
+            }
+        }
+
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun AddTask(navController: NavHostController) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            var title by remember {
+                mutableStateOf("")
+            }
+            var description by remember {
+                mutableStateOf("")
+            }
+            // true and true = true
+            // false and true = false
+            // false and false = false
+            // true and false = false
+            val canProceed by remember {
+                derivedStateOf {
+                    title.isNotEmpty() && description.isNotEmpty()
+                }
+            }
+            Text(
+                text = "New Task",
+                fontWeight = FontWeight.Bold,
+                color = Color(0xff246BFE)
+            )
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .width(320.dp)
+                    .padding(top = 20.dp)
+                    .border(BorderStroke(1.dp, SolidColor(Color.Black))),
+                colors = TextFieldDefaults
+                    .textFieldColors(
+                        containerColor = Color.White,
+                        textColor = Color.Gray,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                value = title,
+                onValueChange = {
+                    title = it
+                },
+                textStyle = TextStyle(
+                    fontWeight = FontWeight.Bold
+                ),
+                placeholder = {
+                    Text("Title")
+                }
+            )
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .width(320.dp)
+                    .height(200.dp)
+                    .padding(top = 20.dp)
+                    .border(BorderStroke(1.dp, SolidColor(Color.Black))),
+                colors = TextFieldDefaults
+                    .textFieldColors(
+                        containerColor = Color.White,
+                        textColor = Color.Gray,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                value = description,
+                onValueChange = {
+                    description = it
+                },
+                textStyle = TextStyle(
+                    fontWeight = FontWeight.Bold
+                ),
+                placeholder = {
+                    Text("Description")
+                }
+            )
+            Button(
+                enabled = canProceed,
+                modifier = Modifier
+                    .padding(horizontal = 36.dp)
+                    .fillMaxWidth()
+                    .height(70.dp)
+                    .padding(top = 20.dp),
+                onClick = {
+                    store.add(Task(++iid.value, title, description))
+                    navController.popBackStack()
+                },
+                shape = RoundedCornerShape(30.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF457EF5)
+                )
+            ) {
+                Text(
+                    text = "Add",
+                    fontWeight = FontWeight.Bold,
+                )
+
+            }
+        }
+    }
+
+    @Composable
+    fun HomePage(navController: NavHostController) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ){
+            LazyColumn(
+
+            ){
+                items(store){
+                    Row(){
+                        Text(
+                            it.title + ":" + it.description,
+                            fontSize = 24.sp,
+                            color = Color.Blue
+                        )
+                        Button(onClick = {
+                            val id = it.id
+                            val item = store.find {
+                                it.id == id
+                            }
+                            store.remove(item)
+                        }) {
+                            Text("Delete")
+                        }
+                    }
+
+                }
+            }
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate("add")
+                },
+                shape = CircleShape,
+                containerColor = Color(0xff00C14D),
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(24.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "",
+                    tint = Color.White
+                )
             }
         }
 
@@ -277,3 +477,11 @@ fun Loader() {
         progress = { progress },
     )
 }
+
+// navigation, back
+// data class
+// list, mutable state list
+// find, remove
+// floating action button
+// derived state of
+// button enable/disable
---
+++ app/src/main/java/com/algogence/ourtodo/mypackage/Task.kt
@@ -0,0 +1,7 @@
+package com.algogence.ourtodo.mypackage
+
+data class Task(
+    val id: Int,
+    val title: String,
+    val description: String
+)
